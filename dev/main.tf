2d01b52c8f9fa253bc95d4f49733aa31d2a86111
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-10-09 21:23:25 +0900

    update dev/main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-northeast-1" # あなたの使用したいリージョンに変更してください
+  region = "ap-northeast-3" # あなたの使用したいリージョンに変更してください
 }
 
 resource "aws_instance" "ansible_host" {
-  ami           = "ami-0e812285fd54f7620" # Red Hat Enterprise Linux 9 (HVM), SSD Volume Type AMI (HVM) のAMI ID, リージョンによって変更してください
+  ami           = "ami-063cd3af637f3e426" # Red Hat Enterprise Linux 9 (HVM), SSD Volume Type AMI (HVM) のAMI ID, リージョンによって変更してください
   instance_type = "t2.micro"
 
   tags = {
