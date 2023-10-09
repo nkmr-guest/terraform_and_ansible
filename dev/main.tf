ed4cca40e48dbe92b0b10f8cd62edaa74b87922d
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-10-09 12:11:01 +0900

    update dev/main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ansible_host" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 LTS AMI (HVM) のAMI ID, リージョンによって変更してください
+  ami           = "ami-0d5c8edc10c17ec35" # Red Hat Enterprise Linux 9 (HVM), SSD Volume Type AMI (HVM) のAMI ID, リージョンによって変更してください
   instance_type = "t2.micro"
 
   tags = {
