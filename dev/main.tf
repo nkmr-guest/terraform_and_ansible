6c3f92c20b0221a10a20bd3a1116cf6d4e6a21e2
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-10-10 13:34:32 +0900

    mod dev/main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,14 +1,19 @@
 provider "aws" {
-  region = "ap-northeast-3" # あなたの使用したいリージョンに変更してください
+  region = "ap-northeast-3"
 }
 
 resource "aws_instance" "ansible_host" {
-  ami           = "ami-063cd3af637f3e426" # Red Hat Enterprise Linux 9 (HVM), SSD Volume Type AMI (HVM) のAMI ID, リージョンによって変更してください
+  ami           = "ami-063cd3af637f3e426"
   instance_type = "t2.micro"
 
   tags = {
     Name = "AnsibleHost"
   }
 
-  key_name = "test_ansible" # あなたのEC2キーペアの名前に変更してください
+  key_name = "test_ansible"
+}
+
+output "instance_ip" {
+  value = aws_instance.ansible_host.public_ip
+  description = "Public IP of the Ansible Host"
 }
