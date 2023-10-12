ee52085430b42a939eaaad6797300ec0f9c1ef95
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-10-12 22:01:34 +0900

    update dev/main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -17,3 +17,13 @@ output "instance_ip" {
   value = aws_instance.ansible_host.public_ip
   description = "Public IP of the Ansible Host"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-unique-bucket-name-6493000954"   # 作成したS3バケットの名前を指定します。
+    key            = "terraform.tfstate"     # S3に保存するステートファイルの名前を指定します。
+    region         = "ap-northeast-1"        # S3バケットが存在するリージョンを指定します。
+    encrypt       = true                    # ステートファイルをS3に保存する際に暗号化するかどうかを指定します（オプション）。
+    # dynamodb_table = "your-dynamodb-table"  # ステートのロックを行うためのDynamoDBテーブル名を指定します（オプション）。
+  }
+}
