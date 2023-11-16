b484fe75c70eca4b78a64f72385da49dcb8c5d00
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-16 12:48:49 +0900

    mod module s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,7 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
   tags = var.tags
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.bucket.id 
-#  acl    = var.bucket_acl
 }
