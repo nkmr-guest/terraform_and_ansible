718d57dd80590e785d3d419b951ff077ad0d035e
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-16 12:52:34 +0900

    mod module s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,7 +1,12 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
-  tags = var.tags
 }
