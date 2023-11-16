88fd8530756980498668c7b73d38fe249f4b1fbd
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-16 10:42:16 +0900

    mod module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "bucket" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
+resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id 
   acl    = var.bucket_acl
 }
