8d3e95171121ba30fcfc51be13a600ac406d0329
Ryosuke Nakamura <k.nakamura.ryosuke@mind.co.jp>
2023-11-17 09:31:37 +0900

    update env/dev backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 // backend.tf
-// terraform {
-//   backend "s3" {
-//     bucket	   = "dev-terraform-bucket-name-3498403"
-//     key            = "terraform.tfstate"
-//     region         = "ap-northeast-1"
-//     encrypt        = true
-//   }
-// }
+terraform {
+  backend "s3" {
+    bucket	   = "dev-terraform-bucket-name-3498403"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-1"
+    encrypt        = true
+  }
+}
