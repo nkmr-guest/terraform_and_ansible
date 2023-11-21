a1e600343cdd544bbfc284e8907ba799a759e73e
Ryosuke Nakamura <k.nakamura.ryosuke@mind.co.jp>
2023-11-21 15:36:51 +0900

    mod env/dev/output.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,8 +1,8 @@
 output "instance_ip" {
-  value       = aws_instance.ansible_host.public_ip
+  value       = module.ansible_host.public_ip
   description = "Public IP of the Ansible Host"
 }
 
 output "instance_private_ip" {
-  value = aws_instance.ansible_host.instance_ip
+  value = module.ansible_host.instance_ip
 }
