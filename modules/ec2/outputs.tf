9abd7c6b74046500f9bd58050c3a499422b86b30
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-23 18:42:02 +0900

    update ec2 module outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -7,3 +7,7 @@ output "instance_private_ip" {
   value       = aws_instance.ansible_host.private_ip
   description = "Private IP of the Ansible Host"
 }
+
+output "sg_id" {
+  value = aws_security_group.ec2_security_group.id
+}
