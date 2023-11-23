59702f315dfd45d487590bc72175c3dfb2baefe9
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-23 18:32:06 +0900

    mod ec2 module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,15 @@
+module "ec2_sg" {
+  source = "../../modules/security_group"
+
+  sg_config = {
+    name        = "ec2_sg"
+    vpc_id      = module.vpc.vpc_id
+    protocol    = "tcp"
+    port        = [22, 80, 443]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "ansible_host" {
   ami           = var.ami
   instance_type = var.instance_type
