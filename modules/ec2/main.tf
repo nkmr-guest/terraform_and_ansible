b86f1aa83dd98ce2c084e3d760b1aab452280aab
Ryosuke Nakamura <k.nakamura.ryosuke@mind.co.jp>
2023-11-24 15:20:00 +0900

    update ec2 module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "ansible_host" {
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [
-    var.sg_id
+    var.ec2_config.sg_id
   ]
-  subnet_id     = var.subnet_id
+  subnet_id = element(var.ec2_config.public_subnet_id, count.index % length(var.ec2_config.public_subnet_id))
   tags = var.tags
 
   key_name = var.key_name
