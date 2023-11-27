543da4c013dbada641ffaddfc9ed172e91ce8132
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-27 13:11:42 +0900

    update module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,13 @@
 resource "aws_instance" "ansible_host" {
+  count = length(var.ec2_config.public_subnet_id) + length(var.ec2_config.private_subnet_id)
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [
     var.ec2_config.sg_id
   ]
-  subnet_ids = flatten([var.ec2_config.public_subnet_id, var.ec2_config.private_subnet_id])
+  subnet_id     = count.index < length(var.ec2_config.public_subnet_id) ? var.ec2_config.public_subnet_id[count.index] : var.ec2_config.private_subnet_id[count.index - length(var.ec2_config.public_subnet_id)]
+
+  #subnet_ids = flatten([var.ec2_config.public_subnet_id, var.ec2_config.private_subnet_id])
   tags = var.tags
 
   key_name = var.key_name
