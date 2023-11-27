1d91adebad2bba50e59e4811c07ddb6ea41a288e
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-27 10:39:50 +0900

    check update

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "ansible_host" {
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [
-    var.ec2_config.sg_id
+    var.sg_id
   ]
-  subnet_id = var.ec2_config.public_subnet_id
+  subnet_id = var.public_subnet_id
   tags = var.tags
 
   key_name = var.key_name
