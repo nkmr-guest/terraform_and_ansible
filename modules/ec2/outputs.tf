44d892cd6cee010250a74cc53ad785a58666db46
ryosuke-nkmr <nkmrrysk_31_58@i.softbank.jp>
2023-11-27 13:14:41 +0900

    update ec2 module outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,10 +1,12 @@
 output "instance_public_ip" {
-  value       = aws_instance.ansible_host.public_ip
+  #value       = aws_instance.ansible_host.public_ip
+  value       = aws_instance.ansible_host[count.index].public_ip
   description = "Public IP of the Ansible Host"
 }
 
 output "instance_private_ip" {
-  value       = aws_instance.ansible_host.private_ip
+  #value       = aws_instance.ansible_host.private_ip
+  value       = aws_instance.ansible_host[count.index].private_ip
   description = "Private IP of the Ansible Host"
 }
 
